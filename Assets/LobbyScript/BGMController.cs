289bc58caa75b16f8c1ea5f379039d0370857686
ATtami YYNa <tami19996@gmail.com>
2020-11-18 11:26:19 +0900

    BGMFIX,SMOKE SE FIX,BREACH SE FIX

---
+++ Assets/LobbyScript/BGMController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LobbyScript
+{
+    public class BGMController : MonoBehaviour
+    {
+        [SerializeField]
+        AudioSource BGMSource;
+        [SerializeField]
+        private float volume = 0.5f;
+        void Start()
+        {
+            BGMSource.volume = volume * UI.SettingPanel.AudioController.InformAudioValue();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            BGMSource.volume = volume * UI.SettingPanel.AudioController.InformAudioValue();
+        }
+    }
+}
