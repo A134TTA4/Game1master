442e16146464665b31d653d422df6341dcaa334c
ATtami YYNa <tami19996@gmail.com>
2021-01-12 20:54:50 +0900

    なんの銃持ってるか分かるようにした

---
+++ Assets/UI/HaveGunDesp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HaveGunDesp : MonoBehaviour
+    {
+        [SerializeField]
+        private Text HaveGunText;
+        void Update()
+        {
+
+            if (TimeManager.MainPhaze.InformMainphaze() == false)
+            {
+                return;
+            }
+
+            if (Player.WeaponSwap.InformWeapon() == false)
+            {
+                HaveGunText.text = "RIFLE\nMAGAZINE";
+            }
+            else
+            {
+                HaveGunText.text = "SIDEARM\nMAGAZINE";
+            }
+        }
+    }
+}
