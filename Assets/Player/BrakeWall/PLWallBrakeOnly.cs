c3c290ac88d35a9be0a273422bdb67984be34558
ATtami YYNa <tami19996@gmail.com>
2021-02-09 13:09:14 +0900

    Smoke BrakeWall Fix

---
+++ Assets/Player/BrakeWall/PLWallBrakeOnly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Player
+{
+    namespace BrakeWall
+    {
+        public class PLWallBrakeOnly : MonoBehaviour
+        {
+            private void OnTriggerEnter(Collider other)
+            {
+                if (other.CompareTag("Cube"))
+                {
+                    Destroy(other.gameObject);
+                }
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
